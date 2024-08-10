96b7e9bdef5da36a000461ea73980ef495da0018
BURIOKA <burioka@BURIOKAnoMacBook-Pro.local>
2024-08-10 15:34:27 +0900

    bs4とターボリンク削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,14 +4,12 @@
 // that code so it'll be compiled.
 //= require_tree .
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
-
+import 'bootstrap'
 import $ from 'jquery';
 window.jQuery = $;
 window.$ = $;
 Rails.start()
-Turbolinks.start()
 ActiveStorage.start()
